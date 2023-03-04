d855b866032631cb9e34eb12a583a7609124972d
Abhishek <abhishekparmar162@gmail.com>
2023-03-04 16:48:51 +0530

    added navbar

--- components/NavBar.jsx
+++ components/NavBar.jsx
@@ -1,10 +1,14 @@
 import Link from "next/link";
-import React from "react";
+import React, { useState } from "react";
 import {FiSearch} from 'react-icons/fi'
-import {AiOutlineMenu} from 'react-icons/ai'
+import {AiOutlineMenu , AiOutlineClose} from 'react-icons/ai'
 const NavBar = () => {
+    const [nav, setNav] = useState(false)
+    const handleNav = () => {
+        setNav(!nav)
+    }
   return (
-    <div className="w-full h-12 flex items-center justify-between m-2">
+    <div className="w-full h-12 flex items-center justify-between m-2 mx-auto">
       <div>
         <h1 className="text-orange-500 text-xl">Abhishek</h1>
       </div>
@@ -13,14 +17,26 @@ const NavBar = () => {
         <input placeholder="Type here..." className=" px-2"/>
       </div>
       <div className=" items-center space-x-3 mr-3 hidden lg:flex">
-        <Link  href={'#'}>Home</Link>
+      <Link  href={'#'}>Home</Link>
+        <Link href={'#'}>Company</Link>
+        <Link href={'#'}>Resources</Link>
         <Link href={'#'}>About</Link>
         <Link href={'#'}>Contact</Link>
-        <Link href={'#'}>Media</Link>
       </div>
-      <div className="lg:hidden m-2">
-<AiOutlineMenu size={30} />
+      <div className="lg:hidden m-2" onClick={handleNav}>
+        {nav ? <AiOutlineClose size={30} /> : <AiOutlineMenu size={30}  />}
+
       </div>
+      {nav ?  <div className="flex absolute top-0 uppercase left-0 space-y-10 h-full p-8  w-64 border-r-2 lg:hidden flex-col">
+     <h1 className="text-orange-500 text-xl">Abhishek</h1>
+
+     <Link  href={'#'}>Home</Link>
+        <Link href={'#'}>Company</Link>
+        <Link href={'#'}>Resources</Link>
+        <Link href={'#'}>About</Link>
+        <Link href={'#'}>Contact</Link>
+     </div>  : ''}
+    
     </div>
   );
 };
